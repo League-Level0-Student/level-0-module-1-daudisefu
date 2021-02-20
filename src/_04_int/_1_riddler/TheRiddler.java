2ea9100ec1505c549b1570717208437390088c13
administrator <administrator@administrator-Inspiron-15-3552>
2021-02-20 04:28:46 -0800

    If you have me, you want to share me. If you share me, you haven't got me. What am I?

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -11,14 +14,48 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
 		int score= 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
+			String input=JOptionPane.showInputDialog(null, "What has many keys but can't open a single lock?");
+		// 4. If hey got the answer right, pop up "correct!" and increase the score by one
+			if(input.equals("keyboard")) {
+				
+			score++;
+				JOptionPane.showMessageDialog(null,"yes it was the right answer ");
+			}
+		// 5. Otherwise, say "wrong" and tell ethem the answer
+			else  {
+				
+				JOptionPane.showMessageDialog(null,"that was wrong the answer is keyboard");
+			}
 		// 6. Add some more riddles
+			
+			input=JOptionPane.showInputDialog(null," What has to be broken before you use it?");
+					if(input.equals("egg")) {
+						score+=1;
+						JOptionPane.showMessageDialog(null,"good job");
+					}
+					
+					else {
+						
+						JOptionPane.showInternalMessageDialog(null, " it one more time  that was wrong that answer is egg");
+					}
+					
+					input=JOptionPane.showInputDialog(null, "Feed me and I live, yet give me a drink and I die");
+							
+						
+							
+							if(input.equals("fire")) {
+								score+=1;
+								JOptionPane.showMessageDialog(null,"that was the best" );		
+								
+							}
+							else {
+								JOptionPane.showMessageDialog(null, "you do better next time the answer is fire");
+							}
+							JOptionPane.showMessageDialog(null, "you score is "+score );
 
 		// 2. Make a pop up to show the score.
+			
+			
 		
 	}
 }
